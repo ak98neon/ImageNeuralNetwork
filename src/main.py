2604cb626ec09f5301b00f00116a3bfed7727e99
ak98neon <artem_kudria@epam.com>
2020-02-01 11:34:13 +0200

    Added dump with train nn, add perfomancer

--- src/main.py
+++ src/main.py
@@ -1,110 +1,55 @@
 import numpy
-import scipy.special
-
-
-# neural network class definition
-class neuralNetwork:
-
-    # initialise the neural network
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
-        # set number of nodes in each input, hidden, output layer
-        self.inodes = inputnodes
-        self.hnodes = hiddennodes
-        self.onodes = outputnodes
-
-        # link weight matrices, wih and who
-        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
-        # w11 w21
-        # w12 w22 etc
-        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
-        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
-
-        # learning rate
-        self.lr = learningrate
-
-        # activation function is the sigmoid function
-        self.activation_function = lambda x: scipy.special.expit(x)
-
-        pass
-
-    # train the neural network
-    def train(self, inputs_list, targets_list):
-        # convert inputs list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        targets = numpy.array(targets_list, ndmin=2).T
-
-        # calculate signals into hidden layer
-        hidden_inputs = numpy.dot(self.wih, inputs)
-        # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
-
-        # calculate signals into final output layer
-        final_inputs = numpy.dot(self.who, hidden_outputs)
-        # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function(final_inputs)
-
-        # output layer error is the (target - actual)
-        output_errors = targets - final_outputs
-        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
-        hidden_errors = numpy.dot(self.who.T, output_errors)
-
-        # update the weights for the links between the hidden and output layers
-        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
-                                        numpy.transpose(hidden_outputs))
-
-        # update the weights for the links between the input and hidden layers
-        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
-                                        numpy.transpose(inputs))
-
-        pass
-
-    # query the neural network
-    def query(self, inputs_list):
-        # convert inputs list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-
-        # calculate signals into hidden layer
-        hidden_inputs = numpy.dot(self.wih, inputs)
-        # calculate the signals emerging from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
-
-        # calculate signals into final output layer
-        final_inputs = numpy.dot(self.who, hidden_outputs)
-        # calculate the signals emerging from final output layer
-        final_outputs = self.activation_function(final_inputs)
-
-        return final_outputs
-
 
 # number of input, hidden and output nodes
+from src.network import NeuralNetwork
+
 input_nodes = 784
-hidden_nodes = 100
+hidden_nodes = 200
 output_nodes = 10
 
-# learning rate is 0.3
-learning_rate = 0.3
+# learning rate is 0.1
+learning_rate = 0.1
 
 # create instance of neural network
-n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+# n = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+n = NeuralNetwork.loadDump('neural_dump.txt')
 
-training_file = open("../mnist_dataset/mnist_train_100.csv", "r")
-training_data_list = training_file.readlines()
-training_file.close()
+# training_file = open("../mnist_dataset/mnist_train.csv", "r")
+# training_data_list = training_file.readlines()
+# training_file.close()
+#
+# epochs = 5
+#
+# for e in range(epochs):
+#     for record in training_data_list:
+#         all_values = record.split(",")
+#         inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+#         targets = numpy.zeros(output_nodes) + 0.01
+#         targets[int(all_values[0])] = 0.99
+#         n.train(inputs, targets)
+#         pass
+# pass
 
-for record in training_data_list:
-    all_values = record.split(",")
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    targets = numpy.zeros(output_nodes) + 0.01
-    targets[int(all_values[0])] = 0.99
-    n.train(inputs, targets)
-    pass
-
-test_data_file = open("../mnist_dataset/mnist_test_10.csv", "r")
+test_data_file = open("../mnist_dataset/mnist_test.csv", "r")
 test_data_list = test_data_file.readlines()
 test_data_file.close()
 
-all_values = test_data_list[0].split(",")
-print(all_values[0])
+storecard = []
 
-query = n.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
-print(query)
+for record in test_data_list:
+    all_values = record.split(',')
+    correct_label = int(all_values[0])
+    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+    outputs = n.query(inputs)
+    label = numpy.argmax(outputs)
+
+    if label == correct_label:
+        storecard.append(1)
+    else:
+        storecard.append(0)
+        pass
+pass
+
+storecard_array = numpy.asarray(storecard)
+print("Performance: ", storecard_array.sum() / storecard_array.size)
+# n.dumpNetwork()
---
+++ src/network.py
@@ -0,0 +1,114 @@
+import numpy
+import scipy.special
+import json
+
+
+class NeuralNetwork:
+
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        # link weight matrices, wih and who
+        # weights inside the arrays are w_i_j, where link is from node i to node j in the next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.normal(0.0, pow(self.inodes, -0.5), (self.hnodes, self.inodes))
+        self.who = numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.onodes, self.hnodes))
+
+        # learning rate
+        self.lr = learningrate
+
+        # activation function is the sigmoid function
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        pass
+
+    # train the neural network
+    def train(self, inputs_list, targets_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        # output layer error is the (target - actual)
+        output_errors = targets - final_outputs
+        # hidden layer error is the output_errors, split by weights, recombined at hidden nodes
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # update the weights for the links between the hidden and output layers
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        numpy.transpose(hidden_outputs))
+
+        # update the weights for the links between the input and hidden layers
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        numpy.transpose(inputs))
+
+        pass
+
+    # query the neural network
+    def query(self, inputs_list):
+        # convert inputs list to 2d array
+        inputs = numpy.array(inputs_list, ndmin=2).T
+
+        # calculate signals into hidden layer
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # calculate the signals emerging from hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # calculate signals into final output layer
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # calculate the signals emerging from final output layer
+        final_outputs = self.activation_function(final_inputs)
+
+        return final_outputs
+
+    def serialize(self):
+        return {
+            "inodes": self.inodes,
+            "hnodes": self.hnodes,
+            "onodes": self.onodes,
+            "lr": self.lr,
+            "who": self.who.tolist(),
+            "wih": self.wih.tolist()
+        }
+
+    def fromJson(self, json_):
+        self.inodes = json_["inodes"]
+        self.hnodes = json_["hnodes"]
+        self.onodes = json_["onodes"]
+        self.lr = json_["lr"]
+        self.who = json_["who"]
+        self.wih = json_["wih"]
+
+    def dumpNetwork(self):
+        with open('neural_dump.txt', 'w') as outfile:
+            json.dump(self.serialize(), outfile)
+
+    @staticmethod
+    def loadDump(fileName):
+        with open(fileName) as json_file:
+            data = json.load(json_file)
+            inodes = data["inodes"]
+            hnodes = data["hnodes"]
+            onodes = data["onodes"]
+            lr = data["lr"]
+            who = data["who"]
+            wih = data["wih"]
+            nw = NeuralNetwork(inodes, hnodes, onodes, lr)
+            nw.who = who
+            nw.wih = wih
+            return nw
+            # return NeuralNetwork.fromJson(data)
